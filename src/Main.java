e046b3b91c46330061633b06ade622ea4b5bce69
albertoam3 <albertoarcones3@gmail.com>
2024-04-16 18:06:00 +0200

    project

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
 
+
         // 1. Definir la consulta SPARQL
         String queryString = "PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
                 "PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
